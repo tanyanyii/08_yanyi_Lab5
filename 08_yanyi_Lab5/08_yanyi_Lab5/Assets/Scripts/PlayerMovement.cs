0e3e0bd24a3bd13b35c3279ac098bca8b1128118
21-Harriz <muhammadharriz27@gmail.com>
2024-01-10 12:43:01 +0800

    Updated Unity Project
    Updated by Harriz

---
+++ 08_yanyi_Lab5/08_yanyi_Lab5/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody playerRigidbody;
+    public GameObject coin;
+    public Text CoinCollected;
+    public int coincount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CoinCollected.text = "Coins Collected = " + coincount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CoinCollected.text = "Coins Collected = " + coincount;
+        if(coincount>=4)
+        {
+            SceneManager.LoadScene("GamePlay_Level2");
+        }
+    }
+    private void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(MoveHorizontal, 0, MoveVertical);
+        transform.Translate(movement * Time.deltaTime * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "coin")
+        {
+            Destroy(other.gameObject);
+            coincount++;
+        }
+        if (other.tag == "hazard")
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+}
---
+++ 08_yanyi_Lab5/08_yanyi_Lab5/Assets/Scripts/PlayerMovement2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement2 : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody playerRigidbody;
+    public GameObject cookie;
+    public Text CookieCollected;
+    public int cookiecount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CookieCollected.text = "Cookies Collected = " + cookiecount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CookieCollected.text = "Cookie Collected = " + cookiecount;
+        if (cookiecount >= 4)
+        {
+            SceneManager.LoadScene("GameWin");
+        }
+    }
+    private void FixedUpdate()
+    {
+        float MoveHorizontal = Input.GetAxis("Horizontal");
+        float MoveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(MoveHorizontal, 0, MoveVertical);
+        transform.Translate(movement * Time.deltaTime * speed);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "cookie")
+        {
+            Destroy(other.gameObject);
+            cookiecount++;
+        }
+        if (other.tag == "hazard")
+        {
+            SceneManager.LoadScene("GameLose");
+        }
+    }
+}
